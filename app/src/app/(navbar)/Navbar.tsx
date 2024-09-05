12c383a8e1f484d941511994adf7c84dc0d798c2
Atakan <158854874+Atakanlift@users.noreply.github.com>
2024-09-05 04:22:33 +0300

    as
    as

--- app/src/app/(navbar)/Navbar.tsx
+++ app/src/app/(navbar)/Navbar.tsx
@@ -49,7 +49,7 @@ const Navbar = () => {
 
             <div className='sm:flex sm:flex-row'>
 
-                <p onClick={logoClick} className='absolute text-[30px] w-[1300px] mt-1 ml-5 font-bold text-[16px] sm:left-[55px] sm:top-[20px] sm:text-[24px] sm:mr-[20px] w-[135px] sm:w-[180px] cursor-pointer'>
+                <p onClick={logoClick} className='absolute text-[30px] w-[220px] mt-1 ml-5 font-bold text-[16px] sm:left-[55px] sm:top-[20px] sm:text-[24px] sm:mr-[20px] sm:w-[200px] sm:w-[180px] cursor-pointer'>
                     AtaRam & Market
                 </p>
 
@@ -96,23 +96,23 @@ const Navbar = () => {
 
 
 
-            <div className='bg-black-500 sm:bg-black  z-3000 w-[483px] h-[78px]  mt-[626px] ml-[0px] text-[20px] fixed sm:w-[500px] sm:h-[50px] sm:absolute sm:mt-[8px] sm:ml-[550px]'>
-                <div className='mt-[-20px] sm:mt-[0px] ml-[50px]'>
-                    <hr className='w-[535px] ml-[-50px] mt-5 fixed sm:hidden' />
-                    <Link className='text-[15px] top-[32px] left-5 relative sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:mr-5 sm:hover:text-blue-400' href="/">
-                        <CiHome className='sm:hidden text-[36px] ml-3 relative' />
+            <div className='bg-black-500     h-[78px]  mt-[626px] ml-[0px] text-[20px]  sm:w-[500px] sm:h-[50px] sm:absolute sm:mt-[8px] sm:ml-[550px]'>
+                <div className='mt-[-20px] sm:mt-[0px] ml-[80px]'>
+                    <hr className='w-[535px] ml-[-80px] mt-5 fixed sm:hidden' />
+                    <Link className='text-[12px] top-[32px] left-5 relative sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:mr-5 sm:hover:text-blue-400' href="/">
+                        <CiHome className='sm:hidden text-[28px] ml-3 relative' />
                         Ana Sayfa
 
                     </Link>
-                    <Link className='text-[15px] top-[-35px] left-[100px] relative sm:relative sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:mr-5 sm:hover:text-blue-400' href="/tüm-ürünler">
-                        <AiOutlineProduct className='sm:hidden text-[36px] left-[20px] top-[0px] relative' />
+                    <Link className='text-[12px] top-[-26px] left-[90px] relative sm:relative sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:mr-5 sm:hover:text-blue-400' href="/tüm-ürünler">
+                        <AiOutlineProduct className='sm:hidden text-[28px] left-[20px] top-[0px] relative' />
                         Tüm Ürünler
                     </Link>
-                    <Link className='relative text-[15px] left-[200px] top-[-102px] sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:hover:text-blue-400' href="/hakkimizda">
-                        <MdArticle className='sm:hidden text-[36px] left-[16px] top-[0px] relative' />
+                    <Link className='relative text-[12px] left-[170px] top-[-84px] sm:top-[15px] sm:left-[20px] sm:text-white sm:font-semibold sm:hover:text-blue-400' href="/hakkimizda">
+                        <MdArticle className='sm:hidden text-[28px] left-[16px] top-[0px] relative' />
                         Hakkımızda</Link>
-                    <Link className='relative top-[-168px] left-[300px] text-[15px] sm:top-[15px] sm:left-[40px] sm:text-white sm:font-semibold sm:hover:text-blue-400' href="/iletisim">
-                        <MdContactEmergency className='sm:hidden text-[36px] left-[5px] top-[0px] relative' />
+                    <Link className='relative top-[-141px] left-[250px] text-[12px] sm:top-[15px] sm:left-[40px] sm:text-white sm:font-semibold sm:hover:text-blue-400' href="/iletisim">
+                        <MdContactEmergency className='sm:hidden text-[28px] left-[5px] top-[0px] relative' />
                         İletişim</Link>
                 </div>
             </div>
